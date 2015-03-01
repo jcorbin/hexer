6391d13c2c328ac14d7eda80841a737c2d653972
Joshua T Corbin <jcorbin@wunjo.org>
2015-02-28 21:42:13 -0800

    ChunkedHexTransform: trivial refactor

--- chunked_hex_transform.js
+++ chunked_hex_transform.js
@@ -13,7 +13,7 @@ function ChunkedHexTransform(options) {
     if (typeof options.header === 'function') {
         self.header = options.header;
     } else {
-        self.header = simpleHeader;
+        self.header = simpleHeader();
     }
     self.chunkNum = 0;
 }
@@ -37,12 +37,15 @@ ChunkedHexTransform.prototype._transform = function transform(chunk, encoding, d
     });
 };
 
-function simpleHeader(chunkNum, chunk) {
-    var len = chunk.length;
-    var hexlen = len.toString(16);
-    return util.format(
-        '-- chunk[%s] length: %s (0x%s)\n',
-        chunkNum, len, hexlen);
+function simpleHeader(opts) {
+    opts = opts || {};
+    return function header(chunkNum, chunk) {
+        var len = chunk.length;
+        var hexlen = len.toString(16);
+        return util.format(
+            '-- chunk[%s] length: %s (0x%s)\n',
+            chunkNum, len, hexlen);
+    };
 }
 
 module.exports = ChunkedHexTransform;
